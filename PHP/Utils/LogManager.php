d669fb4c9116503e6e61ca6ef2365966e2737beb
JonatanPERRET <jonatan.perret@cpnv.ch>
2023-02-20 15:50:00 +0100

    changed path to logs files

--- PHP/Utils/LogManager.php
+++ PHP/Utils/LogManager.php
@@ -1,13 +1,8 @@
 <?php 
     function WriteLog($message, $timeToExecute){
-        $path = "./Logs/";
+        $path = "Logs/";
         $filename = "log_api.txt";
         $fullpath = $path . $filename;
-        // create the file if it doesn't exist
-        if(!file_exists($fullpath)){
-            $logFile = fopen($fullpath, "w");
-            fclose($logFile);
-        }
 
         $message = date("Y-m-d H:i:s") . " - " . $message . " - " . round($timeToExecute, 3) . " seconds";
 
