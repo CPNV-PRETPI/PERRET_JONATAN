207bda35f95423898cae418e4bf79bf9cfcf547d
JonatanPERRET <jonatan.perret@cpnv.ch>
2023-03-08 12:40:59 +0100

    xamarin + securestring  login call

--- FitFocus/FitFocus/FitFocus.iOS/AppDelegate.cs
+++ FitFocus/FitFocus/FitFocus.iOS/AppDelegate.cs
@@ -23,8 +23,10 @@ namespace FitFocus.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
 
+            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+
+            LoadApplication(new App());
             return base.FinishedLaunching(app, options);
         }
     }
--- FitFocus/FitFocus/FitFocus/Views/LoginPage.xaml.cs
+++ FitFocus/FitFocus/FitFocus/Views/LoginPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FitFocus.Services;
 using FitFocus.ViewModels;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +18,25 @@ namespace FitFocus.Views
         {
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
+            scanView.OnScanResult += ScanView_OnScanResult;
+        }
+
+        protected override void OnAppearing()
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                scanView.IsVisible = true;
+                scanView.IsEnabled = true;
+                scanView.IsAnalyzing = true;
+                scanView.IsScanning = true;
+            });
+        }
+
+        private void ScanView_OnScanResult(ZXing.Result result)
+        {
+            // Login system
+            string aa = ApiService.PostToAPIEndpoint_Authentify(result.Text);
+            int i = 0;
         }
     }
 }
