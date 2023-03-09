2832e12818b0599e65178a19bfe5ba0aedb9920b
JonatanPERRET <jonatan.perret@cpnv.ch>
2023-03-09 14:07:27 +0100

    added check if file exist

--- PHP/Utils/LogManager.php
+++ PHP/Utils/LogManager.php
@@ -7,8 +7,13 @@
 
             $message = date("Y-m-d H:i:s") . " - " . $message . " - " . round($timeToExecute*1000, 3) . " ms";
 
-            $logFile = fopen($fullpath, "w+");
-            fwrite($logFile, $message . "\n");
+            if(file_exists($fullpath)){
+                $logFile = fopen($fullpath, "a+") or die("Can't open log file");
+                fwrite($logFile, $message . "\n");
+            }else{
+                $logFile = fopen($fullpath, "w");
+                fwrite($logFile, $message . "\n");
+            }
             fclose($logFile);
         }catch (Exception $e){
             print($e);
